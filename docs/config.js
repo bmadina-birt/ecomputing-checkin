2e7b69862dd4ac6a43f0f5e1954e4d7f9c9055d0
bmadina-birt <67912572+bmadina-birt@users.noreply.github.com>
2026-06-09 08:41:32 +0200

    Update API_URL to point to the new Apps Script

--- docs/config.js
+++ docs/config.js
@@ -2,7 +2,7 @@
 // En la versión final, pega aquí la URL /exec del despliegue de Apps Script.
 // La URL no se muestra en la interfaz de recepción.
 window.ECOMPUTING_CONFIG = {
-  API_URL: 'PEGA_AQUI_LA_URL_EXEC_DE_APPS_SCRIPT',
+  API_URL: 'https://script.google.com/a/macros/birt.eus/s/AKfycbxW5U8Kfw7ROaSFS5Y7T5dFhekCV9oivpSrT6owY_JvPaDMEMoy__jOqN93Gf4-gicM/exec',
   DEFAULT_OPERATOR: 'Recepción 1',
   DEFAULT_DAY: 'jueves',
   EVENT_TITLE: 'eComputing 2026'
